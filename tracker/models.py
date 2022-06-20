ba420a45313a30502ebdee0f699d9ae5e78b68e1
Macias92 <maciejstys92@gmail.com>
2022-06-20 22:19:03 +0200

    create models, but need to be clarified

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Purchaser(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Referred(models.Model):
+    # REFERRERS = (
+    #     (0, 'Get Mortgage'),
+    #     (1, 'Edinburgh'),
+    #     (2, 'Glasgow'),
+    #     (3, 'Keller Williams'),
+    #     (4, 'Right Advice'),
+    #     (5, 'CP'),
+    #     (6, 'AP'),
+    #
+    # )
+    # name = models.IntegerField(choices=REFERRERS)
+    #
+    # def __str__(self):
+    #     return self.get_name_display()
+
+    name = models.CharField(max_length=64)
+
+
+class Case(models.Model):
+    address = models.CharField(max_length=256)
+    purchaser = models.ForeignKey(Purchaser, on_delete=models.CASCADE)
+    offer_accepted = models.DateField()
+    concluded = models.DateField()
+    date_of_entry = models.DateField()
+    title = models.CharField(max_length=32)
+    referred = models.ForeignKey(Referred, on_delete=models.CASCADE)
+
